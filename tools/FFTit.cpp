0f2711ae98afc717195d65fc8ed717f52022007f
Paul <paul@epicyclism.com>
2026-08-10 10:59:14 +0100

    Correct error output to actually stderr....

--- tools/FFTit.cpp
+++ tools/FFTit.cpp
@@ -10,6 +10,7 @@
 #include <charconv>
 
 #include <fmt/format.h>
+#include <fmt/ostream.h>
 
 #include "fftlib.h"
 #include "mm_file.h"
@@ -21,28 +22,28 @@ template <typename T> void from_chars(char const* arg, T& result)
 
 void welcome()
 {
-	fmt::println("FFTit 2.00\n");
-	fmt::println("Performs FFT on a file of raw sample data");
-	fmt::println("(sizeof fp type is {})", sizeof(fp_t));
+	fmt::println(std::cerr,"FFTit 2.00\n");
+	fmt::println(std::cerr,"Performs FFT on a file of raw sample data");
+	fmt::println(std::cerr,"(sizeof fp type is {})", sizeof(fp_t));
 }
 
 void usage()
 {
-	fmt::println("Performs FFTs on a file of raw sample data");
-	fmt::println("Usage : FFTit [-Fn] [-D] [-1] [-Wn] <input file> [sample rate]");
-	fmt::println("Where input file is a packed array of floats. Output is text to stdout.");
-	fmt::println("Options. -Fn, use an FFT width of 2^n.");
-	fmt::println("              n between 8 for 256 and 24 for 16777216.");
-	fmt::println("              Default is 18 for 262144");
-	fmt::println("         -D,  output in dB scaled so 1.0 is 0dB");
-	fmt::println("         -1,  perform a single FFT on the centre FFT width samples of the");
-	fmt::println("              input, otherwise (default) process the entire file and average");
-	fmt::println("              the results of each FFT.");
-	fmt::println("         -Wn, select a window function. 0 is no window.");
-	fmt::println("				1 is Hamming and the default.");
-	fmt::println("              2 is Blackman, 3 Blackman-Harris.");
-	fmt::println("              4 is Kaiser5,  5 Kaiser7.");
-	fmt::println("And if you provide the sample rate, the centre frequencies of each bin are written to the output.");
+	fmt::println(std::cerr,"Performs FFTs on a file of raw sample data");
+	fmt::println(std::cerr,"Usage : FFTit [-Fn] [-D] [-1] [-Wn] <input file> [sample rate]");
+	fmt::println(std::cerr,"Where input file is a packed array of floats. Output is text to stdout.");
+	fmt::println(std::cerr,"Options. -Fn, use an FFT width of 2^n.");
+	fmt::println(std::cerr,"              n between 8 for 256 and 24 for 16777216.");
+	fmt::println(std::cerr,"              Default is 18 for 262144");
+	fmt::println(std::cerr,"         -D,  output in dB scaled so 1.0 is 0dB");
+	fmt::println(std::cerr,"         -1,  perform a single FFT on the centre FFT width samples of the");
+	fmt::println(std::cerr,"              input, otherwise (default) process the entire file and average");
+	fmt::println(std::cerr,"              the results of each FFT.");
+	fmt::println(std::cerr,"         -Wn, select a window function. 0 is no window.");
+	fmt::println(std::cerr,"              1 is Hamming and the default.");
+	fmt::println(std::cerr,"              2 is Blackman, 3 Blackman-Harris.");
+	fmt::println(std::cerr,"              4 is Kaiser5,  5 Kaiser7.");
+	fmt::println(std::cerr,"And if you provide the sample rate, the centre frequencies of each bin are written to the output.");
 }
 
 int main(int argc, char* argv[])
@@ -85,7 +86,7 @@ int main(int argc, char* argv[])
 				wt = wt_from_code(*(argv[arg] + 2));
 				break;
 			default:
-				fmt::print(stderr, "Unknown argument \'{:c}\'!\n", argv[arg][1]);
+				fmt::println(std::cerr, "Unknown argument \'{:c}\'!", argv[arg][1]);
 				usage();
 				return -1;
 			}
@@ -106,20 +107,20 @@ int main(int argc, char* argv[])
 	// checks
 	if (sample_rate == 0)
 	{
-		fmt::println("Sample rate provided was not understood");
+		fmt::println(std::cerr, "Sample rate provided was not understood");
 		usage();
 		return -1;
 	}
 	if (fftWidth < FFTWdMin || fftWidth > FFTWdMax)
 	{
-		fmt::println("FFTWidth provided is out of range, valid between {:d} and {:d} inclusive.", FFTWdMin, FFTWdMax);
+		fmt::println(std::cerr, "FFTWidth provided is out of range, valid between {:d} and {:d} inclusive.", FFTWdMin, FFTWdMax);
 		usage();
 		return -1;
 	}
 	mem_map_file<fp_t> mmf(argv[nInFileArg]);
 	if (!mmf)
 	{
-		fmt::println("Couldn't open <{}>", argv[nInFileArg]);
+		fmt::println(std::cerr, "Couldn't open <{}>", argv[nInFileArg]);
 		return -1;
 	}
 
@@ -128,13 +129,13 @@ int main(int argc, char* argv[])
 	std::vector<fp_t> mean(pfft->width());
 
 	// report
-	fmt::println("FFTit. Processing,  width {:d}, window {}", pfft->width(), wt_to_string(wt));
+	fmt::println(std::cerr, "FFTit. Processing,  width {:d}, window {}", pfft->width(), wt_to_string(wt));
 
 	if (bOnce)
 	{
 		if (mmf.length() < pfft->width())
 		{
-			fmt::println("Insufficient signal supplied for the specified FFT width");
+			fmt::println(std::cerr, "Insufficient signal supplied for the specified FFT width");
 			return -1;
 		}
 		size_t offset = (mmf.length() - pfft->width()) / 2;
@@ -156,7 +157,7 @@ int main(int argc, char* argv[])
 			nffts = 1;
 		else
 		{
-			fmt::println("Insufficient signal supplied for the specified FFT width");
+			fmt::println(std::cerr, "Insufficient signal supplied for the specified FFT width");
 			return -1;
 		}
 
